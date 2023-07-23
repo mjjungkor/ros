1ab49654342d9ad5e7cfed74586ee1922de692f1
mjjung <mjjungkor@naver.com>
2023-07-23 01:31:27 -0700

    1st Subscrib

--- mj_pkg/mj_pkg/simplesubscriber.py
+++ mj_pkg/mj_pkg/simplesubscriber.py
@@ -2,9 +2,9 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 
-class Sim_pub(Node):
+class Sim_sub(Node):
     def __init__(self):
-        super().__init__('simple_pub')
+        super().__init__('simple_sub')
         #DDS에는 string type이 없어 'from std_msgs.msg import String' 진행
         #10은 메시지를 받을 buffer의 크기
         self.pub=self.create_subscription(String, 'listen_message', self.subcallback, 10)        
@@ -18,7 +18,7 @@ class Sim_pub(Node):
 
 def main():
     rclpy.init()
-    node=Sim_pub()
+    node=Sim_sub()
         
     try:
         rclpy.spin(node)
